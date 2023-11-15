f8bdb8bfe40fa90d48970eea133833b9488144fc
diogopcam <134295616+diogopcam@users.noreply.github.com>
2023-11-15 12:25:42 -0300

    Construtores, getters e setters da classe Evento e suas subclasses.

--- Terremoto.java
+++ Terremoto.java
@@ -1,5 +1,17 @@
 public class Terremoto extends Evento {
-
 	private double magnitude;
+	// MAGNITUDE UTILIZA A ESCALA RICHTER
 
+	public Terremoto(String codigo, String data, double latitude, double longitude, double magnitude){
+		super(codigo, data, latitude, longitude);
+		this.magnitude = magnitude;
+	}
+
+	public void setMagnitude(double magnitude) {
+		this.magnitude = magnitude;
+	}
+
+	public double getMagnitude() {
+		return magnitude;
+	}
 }
